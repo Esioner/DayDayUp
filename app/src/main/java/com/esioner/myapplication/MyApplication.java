07fa62a20fa60d54a420aa3079d585584ab13f53
Esioner <727240908@qq.com>
2017-08-22 12:01:29 +0800

    接口及参数的拼接

--- app/src/main/java/com/esioner/myapplication/MyApplication.java
+++ app/src/main/java/com/esioner/myapplication/MyApplication.java
@@ -1,17 +1,25 @@
 package com.esioner.myapplication;
 
 import android.app.Application;
+import android.content.Context;
+import android.graphics.Point;
+import android.os.Build;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.WindowManager;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.jar.Manifest;
 
 
 public class MyApplication extends Application {
+    public static Context mContext;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        mContext = this;
 
     }
 
@@ -23,4 +31,78 @@ public class MyApplication extends Application {
         Log.e("Date", "replaceFragment: " + s);
         return s;
     }
+
+    /**
+     * 获取设备型号
+     */
+    public static String getDeviceModel() {
+        return Build.MODEL;
+    }
+
+    /**
+     * 获取手机制造商
+     */
+    public static String getManufacturer() {
+        return Build.MANUFACTURER;
+    }
+
+    /**
+     * 获取当前 Unix 时间戳
+     */
+    public static long getUnixTime() {
+        return (System.currentTimeMillis() / 1000);
+    }
+
+    /**
+     * 获取屏幕宽度
+     */
+    public static long getScreenWidth() {
+        WindowManager wm = (WindowManager) mContext.getSystemService(WINDOW_SERVICE);
+        Point point = new Point();
+        wm.getDefaultDisplay().getSize(point);
+        int height = point.y;
+        return point.x;
+    }
+
+    /**
+     * 获取屏幕高度
+     *
+     * @return
+     */
+    public static long getScreenHeight() {
+        WindowManager wm = (WindowManager) mContext.getSystemService(WINDOW_SERVICE);
+        Point point = new Point();
+        wm.getDefaultDisplay().getSize(point);
+        return point.y;
+    }
+
+    //返回设备id
+    public static String getDeviceId() {
+        return Build.SERIAL;
+    }
+
+    /**
+     * 返回版本号
+     */
+    public static String getVersionCode() {
+        return Build.VERSION.RELEASE;
+    }
+
+    public static int getOSCode(){
+        return Build.VERSION.SDK_INT;
+    }
+    public static int getScreenDPI(){
+        WindowManager wm = (WindowManager) mContext.getSystemService(WINDOW_SERVICE);
+        DisplayMetrics metric = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(metric);
+        return metric.densityDpi;
+    }
+    /**
+     * 去除小数点
+     */
+    public static String removePoint(String str) {
+        String s = str.replace(".", "");
+        return s;
+    }
+
 }
---
+++ app/src/main/java/com/esioner/myapplication/neihan/_URL.java
@@ -0,0 +1,78 @@
+package com.esioner.myapplication.neihan;
+
+import com.esioner.myapplication.MyApplication;
+
+public class _URL {
+    /**
+     * 获取 content_type
+     */
+    public static String URL_CONTENT_TYPE = "http://lf.snssdk.com/neihan/service/tabs/?";
+    /**
+     * 获取推荐
+     * 获取视频也是此 URL
+     * 获取图片
+     * 获取段子
+     */
+    public static String URL_RECOMMEND = "http://iu.snssdk.com/neihan/stream/mix/v1/?";
+    /**
+     *获取段友秀
+     */
+    public static String URL_FRIEND_SHOW = "http://iu.snssdk.com/neihan/stream/mix/v1/";
+    /**
+     * 获取直播
+     */
+    public static String URL_PLAY_LIVE = "http://hotsoon.snssdk.com/hotsoon/feed/";
+
+
+    /**
+     * 拼接字符串
+     * @param contentType   从获取 content_type 中获取得到的 list_id 字段值。
+     * @param count         返回数量
+     * @param minTime       上次更新时间的 Unix 时间戳，秒为单位
+     * @param iid           一个长度为10的纯数字字符串，用于标识用户唯一性
+     * @param uuid          用户 id，一个长度为15的纯数字字符串
+     * @param openudid      一个长度为16的数字和小写字母混合字符串
+     * @return
+     */
+    public static String getJointUrlParameter(String contentType, String count, String minTime,
+                                              int iid, String uuid, String
+                                                      openudid) {
+        String suffixString = "webp=1" +          //固定值 1
+                "&essence=1" +      //固定值 1
+                "&content_type=" + contentType +    //从获取 content_type 中获取得到的 list_id 字段值。
+                "&message_cursor=-1" + //固定值-1
+                "&am_longitude=" +//经度。可为空
+                "&am_latitude=" +//纬度。可为空
+                "&am_city=" +//城市名，例如：北京市。可为空
+                "&am_loc_time=" + MyApplication.getUnixTime() +//当前时间 Unix 时间戳，毫秒为单位
+                "&count=" + count +//返回数量
+                "&min_time=" + minTime +//上次更新时间的 Unix 时间戳，秒为单位
+                "&screen_width=" + MyApplication.getScreenWidth() +//屏幕宽度，px为单位
+                "&double_col_mode=0" +//固定值0
+                "&iid=" + iid +//一个长度为10的纯数字字符串，用于标识用户唯一性
+                "&device_id=" + MyApplication.getDeviceId() +//设备 id，一个长度为11的纯数字字符串
+                "&ac=wifi"  +//网络环境，可取值 wifi
+                "&channel = 360" +//下载渠道，可360、tencent等
+                "&aid = 7" +//固定值7
+                "&app_name=joke_essay" +//固定值joke_essay
+                "&versionCode=" + MyApplication.removePoint(MyApplication.getVersionCode
+                ()) +//版本号去除小数点，例如612
+                "&version_name=" + MyApplication.getVersionCode() +//版本号，例如6.1.2
+                "&version_name=android" +//设备平台，android 或 ios
+                "&ssmix=a" +//固定值 a
+                "&device_type" + MyApplication.getDeviceModel() +//设备型号，例如 hongmi
+                "&device_brand=" + MyApplication.getManufacturer() +//设备品牌，例如 xiaomi
+                "&os_api=" + MyApplication.getOSCode() +//设备品牌，例如 xiaomi
+                "&os_version=" + MyApplication.getVersionCode() +//操作系统版本号，例如7.1.0
+                "&uuid=" + uuid +//用户 id，一个长度为15的纯数字字符串
+                "&openudid=" + openudid +//一个长度为16的数字和小写字母混合字符串
+                "&manifest_version_code=" + MyApplication.removePoint(MyApplication
+                .getVersionCode()) +//版本号去除小数点，例如612
+                "&resolution=" + MyApplication.getScreenHeight() + "*" + MyApplication
+                .getScreenWidth() +//屏幕宽高，例如 1920*1080
+                "&dpi=" + MyApplication.getScreenDPI() +//手机 dpi
+                "&update_version_code=" + MyApplication.removePoint(MyApplication
+                .getVersionCode()) + "0";//版本号去除小数点后乘10，例如6120
+        return suffixString;
+    }
+}
